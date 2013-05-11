414e2e72ca1d19604870527cdd737a3aca748420
Tai Nguyen Bui <tnb1990@gmail.com>
2013-05-11 11:14:11 +0100

    Using gitHub message

--- RaspberryPi/src/GitTest.java
+++ RaspberryPi/src/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Second test of JAVA code in GitHub");
+		System.out.println("Using GitHub");
 
 	}
 
